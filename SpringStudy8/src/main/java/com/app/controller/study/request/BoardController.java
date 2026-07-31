203fef140ef7e78f6d8e29ad9a6b9e8aed0c24c5
coehdgk0330 <coehdgk0330@gmail.com>
2026-07-31 15:22:12 +0900

    Request 요청 URL 맵핑(컨트롤러), HTTP요청 (GET/POST)

---
+++ SpringStudy8/src/main/java/com/app/controller/study/request/BoardController.java
@@ -0,0 +1,22 @@
+package com.app.controller.study.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@GetMapping("/notice")
+	public String notice() {
+		return "board/notice";
+	}
+	
+	@GetMapping("/faq")
+	public String faq() {
+		return "board/faq";
+	}
+
+
+}
