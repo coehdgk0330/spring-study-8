98ca3093bfa7e00f8fded404eafc1a0b6cd6634a
coehdgk0330 <coehdgk0330@gmail.com>
2026-07-31 16:55:21 +0900

    퀴즈0 QuizProject 생성 및 요청경로 맵핑

---
+++ QuizProject/src/main/java/com/app/controller/QuizController.java
@@ -0,0 +1,25 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class QuizController {
+	
+	@RequestMapping("/main")
+	public String main() {
+		return "main";
+	}
+	
+	@RequestMapping("/product")
+	public String product() {
+		return "product";
+	}
+	
+	@RequestMapping("/mypage")
+	public String mypage() {
+		return "mypage";
+	}
+
+}
