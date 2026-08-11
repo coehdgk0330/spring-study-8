a42640a89e646bee252f5e64ca63685e0f83e84c
coehdgk0330 <coehdgk0330@gmail.com>
2026-08-11 15:14:41 +0900

    MVC 구조 Controller, Service, DAO 계층구조 패턴 구성

---
+++ SpringStudy8/src/main/java/com/app/controller/room/RoomController.java
@@ -0,0 +1,34 @@
+package com.app.controller.room;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.app.dto.room.Room;
+import com.app.service.room.RoomService;
+
+@Controller
+public class RoomController {
+	
+	@Autowired
+	RoomService roomService;
+	
+	
+	// 보유한 모든 호실정보 조회 페이지
+	@GetMapping("/rooms")
+	public String rooms(Model model) {
+		
+		//호실정보
+		//Controller -> Service -> DAO <-> DB
+		
+		List<Room> roomList = roomService.findRoomList();
+		
+		// view 에 호실목록 전달
+		model.addAttribute("roomList", roomList);
+		
+		return "room/rooms";
+	}
+}
